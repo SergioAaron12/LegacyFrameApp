ee49e27eb715078908ccf7bccb05669bb6c5a712
Nacho <i.gomezo@duocuc.cl>
2025-10-21 02:29:18 -0300

    Agregando todo lo visto en clases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,13 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    //librerias nuevas
+    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    // Material icons (necesarios para Visibility / VisibilityOff)
+    implementation("androidx.compose.material:material-icons-extended")
 }
--- app/src/main/java/com/example/legacyframeapp/ui/screen/loginScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.example.legacyframeapp.ui.screen
-
